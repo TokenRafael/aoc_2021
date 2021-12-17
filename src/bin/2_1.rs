d414bd2bf40d838f1bd4c53c348638ccba755328
tokenrafael <rafaeloliveira@tokenlab.com.br>
2021-12-17 18:39:04 -0300

    Day 2 complete

---
+++ src/bin/2_1.rs
@@ -0,0 +1,54 @@
+use std::{str::FromStr, num::ParseIntError};
+
+struct Sub {
+  hor: i32,
+  depth: i32
+}
+
+impl Sub {
+  fn new() -> Self {
+    Sub { hor: 0, depth: 0 }
+  }
+
+  fn movement(&mut self, m: Move) {
+    use Move::{Fwd, Down, Up};
+    match m {
+      Fwd(x) => self.hor += x,
+      Down(x) => self.depth += x,
+      Up(x) => self.depth -= x,
+    };
+  }
+}
+
+enum Move {
+  Fwd(i32),
+  Down(i32),
+  Up(i32)
+}
+
+impl FromStr for Move {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use Move::{Fwd, Down, Up};
+
+        let (cmd, val) = s.split_once(" ").unwrap();
+
+        match cmd {
+            "forward" => Ok(Fwd(val.parse()?)),
+            "down" => Ok(Down(val.parse()?)),
+            "up" => Ok(Up(val.parse()?)),
+            _ => unreachable!()
+        }
+    }
+}
+
+fn main() {
+    let mut sub = Sub::new();
+    let input = include_str!("../../inputs/2_1.txt");
+    for line in input.lines() {
+        let cmd = Move::from_str(line).unwrap();
+        sub.movement(cmd);
+    }
+    println!("{}", sub.hor * sub.depth);
+}
